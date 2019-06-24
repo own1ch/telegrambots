5653142525f77f5337e4e102b82419627b9a77c0
own1ch <xzi123@mail.ru>
2019-06-24 20:31:02 +0500

    Initial commit

---
+++ src/main/java/Bot.java
@@ -0,0 +1,32 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    public void onUpdateReceived(Update update) {
+        String message = update.getMessage().getText();
+        sendMsg(update.getMessage().getChatId().toString(), message);
+    }
+
+    public synchronized void sendMsg(String chatId, String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+        try {
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e) {
+
+        }
+    }
+
+    public String getBotUsername() {
+        return "nomera_kuplyaprodaja_bot";
+    }
+
+    public String getBotToken() {
+        return "846951660:AAEomYrbAEiRmfNFJ0hAOMz16Sm1DaygE7g";
+    }
+}
